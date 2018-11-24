e30d7aac37a1f0de7a488430e8a9bf6b08fb6af3
dstepanov1630 <dstepanov1630@gmail.com>
2018-11-24 17:44:57 +0300

    Ready list and dict

--- projects/lesson1/listsanddicts.py
+++ projects/lesson1/listsanddicts.py
@@ -13,3 +13,15 @@ print(f'Last: {l[-1]}')
 print(l[1:4])
 l.remove('Python')
 #print(l)
+
+#Создайте такой словарь: ...
+
+l = {'city': "Moscow", 'temperature': '20'}
+print(l.get('city'))
+l['temperature'] = str(int(l['temperature']) - 5)
+print(l)
+print(l.__contains__('country'))
+print(l.get('country', 'Russia'))
+l['date'] = '27.05.2017'
+#print(l)
+print(len(l))
