17d053136c6f2d19e372d1cece47f8e57e222b68
dstepanov1630 <dstepanov1630@gmail.com>
2018-11-25 12:46:50 +0300

    Add logging.

--- projects/lesson1/bot/app/bot.py
+++ projects/lesson1/bot/app/bot.py
@@ -1,16 +1,26 @@
 from config import Config
 from telegram.ext import Updater, CommandHandler
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 #print()
 
 def greet_user(bot, update):
     #print(f'Вызван /start {update}')
     text = ('Вызван /start')
+    logging.info(text)
     update.message.reply_text(text)
 
 
 def main():
     mybot = Updater(Config.TOKEN, request_kwargs=Config.PROXY)
+
+    logging.info('Bot starting')
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
