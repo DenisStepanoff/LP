69bfa72932b01b0365ad2dca297e475f3fd8403f
dstepanov1630 <dstepanov1630@gmail.com>
2018-11-25 13:33:15 +0300

    Added echo-bot and logging users messages.

--- projects/lesson1/bot/app/bot.py
+++ projects/lesson1/bot/app/bot.py
@@ -1,5 +1,5 @@
 from config import Config
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 
@@ -16,6 +16,12 @@ def greet_user(bot, update):
     logging.info(text)
     update.message.reply_text(text)
 
+def talk_to_me(bot, update):
+    user_text = (f'Привет, {update.message.chat.first_name}. Ты написал: {update.message.text}')
+    #print(update.message)
+    logging.info(f'User: {update.message.chat.first_name} {update.message.chat.last_name}, Chat ID: {update.message.chat.id}, Message: {update.message.text}')
+    update.message.reply_text(user_text)
+
 
 def main():
     mybot = Updater(Config.TOKEN, request_kwargs=Config.PROXY)
@@ -23,7 +29,9 @@ def main():
     logging.info('Bot starting')
 
     dp = mybot.dispatcher
+
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
     mybot.idle()
