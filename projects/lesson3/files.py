0d30ae5ad14a33d2277edf83c580b0b131a3495a
dstepanov1630 <dstepanov1630@gmail.com>
2018-12-12 21:44:52 +0300

    Correction work 3.

--- projects/lesson3/files.py
+++ projects/lesson3/files.py
@@ -1,10 +1,11 @@
 file_len = 0
 words_num = 0
-with open('referat.txt', 'r', encoding='utf-8') as my_file, open('referat2.txt', 'w', encoding='utf-8') as my_file2:
-	for file_line in my_file:
-		file_len += len(file_line)
-		words_num += len(file_line.split(' '))
-		my_file2.write(file_line.replace('.', '!'))
+with open('referat.txt', 'r', encoding='utf-8') as my_file:
+    with open('referat3.txt', 'w', encoding='utf-8') as my_file2:
+        for file_line in my_file:
+            file_len += len(file_line)
+            words_num += len(file_line.split(' '))
+            my_file2.write(file_line.replace('.', '!'))
 
 print(f'Длина содержимого файла: {file_len}')
 print(f'Количество слов: {words_num}')
