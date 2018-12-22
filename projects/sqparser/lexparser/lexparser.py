a5e78f17b5129f725bd23d8958b6eb310b07b2d8
dstepanov1630 <dstepanov1630@gmail.com>
2018-12-22 22:07:03 +0300

    Add comments.

--- projects/sqparser/lexparser/lexparser.py
+++ projects/sqparser/lexparser/lexparser.py
@@ -22,41 +22,41 @@ class AccessLogParser:
 
     def __lexic_analyzer(self, rules):
 
-        re_prep = [(re.compile(regexp), token_type) for regexp, token_type in rules]
+        re_prep = [(re.compile(regexp), token_type) for regexp, token_type in rules]  # compile RE
 
-        def __lex(line):
+        def __lex(line):  #  function for recursion
             stop_position = len(line) 
             current_position = 0          
-            while current_position < stop_position:
-                for pattern, token_type in re_prep:  
-                    match = pattern.match(line, current_position)    
-                    if match is None:                 
+            while current_position < stop_position:  
+                for pattern, token_type in re_prep:  # try RE one by one
+                    match = pattern.match(line, current_position)  # check if the RE matches the string from position current_position
+                    if match is None:                # if no match, continue with next RE            
                         continue
-                    current_position = match.end()                  
-                    yield (match, token_type)         
-                    break                            
+                    current_position = match.end()   # shift current position                
+                    yield (match, token_type)        # return the found token   
+                    break                            # proceed to the analysis of the remaining line
             
         return __lex
 
 
     def __syntax_analyzer(self, line):
 
-        lexer = self.__lexic_analyzer(self.RE_RULES)
-        tokens = lexer(line)
+        lexer = self.__lexic_analyzer(self.RE_RULES)  # call recursion function
+        tokens = lexer(line)                          # start recursion string lexic analize
         field_index = 0
 
-        for re_match, token_type in tokens:
+        for re_match, token_type in tokens:           # cicle for syntax analize
             
             if token_type == self.DELIMITER:
-                continue  
+                continue                              # skip delimiters
             elif token_type ==  self.EMPTY_DATA:
-                value = None  # 
+                value = None                          # EMPTY_DATA replace with None 
             elif token_type == self.RAW:
-                value = re_match.group(1) 
+                value = re_match.group(1)             # return RAW groupp
             else:
-                raise SyntaxError("Unknown token", token_type, re_match)
+                raise SyntaxError("Unknown token", token_type, re_match)  # if unknown token, raise error
 
-            field_name = self.LOG_FIELDS[field_index]
+            field_name = self.LOG_FIELDS[field_index] # return field name by field index
             field_index += 1
 
             print('{},{}'.format(field_name, value))
