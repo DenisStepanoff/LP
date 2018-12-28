5a15df361c7754a09c12fc26a25b46b373bdcf11
dstepanov1630 <dstepanov1630@gmail.com>
2018-12-28 15:32:54 +0300

    Naming correction.

--- projects/sqparser/pd_parser.py
+++ projects/sqparser/pd_parser.py
@@ -3,16 +3,16 @@ import requests
 
 
 delimiter = '\s+'                         # delimiter in log file
-chunk_size = 15                           # memory save with chunk read; in prod chunk_size=20000
-log_file_name = 'access_big.log'          # log file path
+chunk_size = 15                           # memory saving with chunk read; in prod chunk_size=20000
+log_file_name = 'access.log'          # log file path
 csv_file_name = 'parsed.csv'              # csv file path
-url = 'http://127.0.0.1:8000/file/upload/'# url django rest framework
+url = 'http://127.0.0.1:8000/file/upload/'# url in django rest framework
 
 # parse log file and create csv file with parsed data
 def get_csv(log_file_name, csv_file_name):
 
-    data_file = pd.read_csv(log_file_name, sep=delimiter, header=None, chunksize=chunk_size)
-    for chunk in data_file:
+    data_frame = pd.read_csv(log_file_name, sep=delimiter, header=None, chunksize=chunk_size)
+    for chunk in data_frame:
       chunk.to_csv(csv_file_name, header=None, mode='a') # mode='a' - append mode
 
 # upload csv into django web site with django REST framework 
@@ -26,7 +26,7 @@ def upload_file(csv_file_name):
 def main():
 
     get_csv(log_file_name, csv_file_name)
-    upload_file(csv_file_name)
+    #upload_file(csv_file_name)
 
 
 if __name__ == '__main__':
